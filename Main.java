3aa75d4c423493f731a70713bc288905723c1b23
gzafra267-jpg <gzafra267@unab.edu.co>
2026-02-24 17:19:17 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+                          edad=int(input("¿Cúal es tu edad? : "))
+                            if edad>=18:
+                               print("Usted es mayor de edad")
+                            else:
+                              print("Usted es menor de edad")
+ 
+*******************************************************************************/
+import java.util.*;// es para que se agregen las liberias que se encuntre util
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("¿Cual es tu edad?");//impresion
+		Scanner edad = new Scanner(System.in);//creando un input
+		int age = edad.nextInt();  // nextline sirve para leer strin, nextInt sirve para leer enteros, nextfloat para (decimales)
+		
+		if (age>=18){
+		    System.out.println("eres mayor de edad");
+		}else{
+		    System.out.println("eres menor de edad");
+		  
+		}
+		
+		if (age < 10){
+		    System.out.println("aún eres un niño");
+		}else{
+		    if (age>=10 && age<14){
+		       System.out.println("eres un preadolecente");
+		    }else{
+		        if (age>=14 && age<18){
+		          System.out.println("eres un adulto joven ");
+                }else{
+                   if (age>=18 && age<30){
+                      System.out.println("eres un adulto");
+                       
+                   } 
+		    }
+		    } 
+		}
+		}
+	}
